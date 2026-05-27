6149e65bdf247e92b0228b0c4252c737b462b8cf
DmtrKln <kalnitskiy8@gmail.com>
2026-05-27 18:05:21 +0600

    update

---
+++ src/js/components/_main.js
@@ -0,0 +1,35 @@
+const reservationSwiper = new Swiper(".reservation__slider", {
+
+  slidesPerView: 1,
+  spaceBetween: 20,
+  speed: 800,
+  watchSlidesProgress: true,
+
+
+  navigation: {
+    prevEl: ".reservation__arrowLeft",
+    nextEl: ".reservation__arrowRight",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+
+  },
+
+  on: {
+    init() {
+      updateProgress(this, ".reservation__progress-line");
+      paintCards();
+    },
+
+    slideChange() {
+      updateProgress(this, ".reservation__progress-line");
+    },
+
+    resize() {
+      updateProgress(this, ".reservation__progress-line");
+    },
+  },
+});
